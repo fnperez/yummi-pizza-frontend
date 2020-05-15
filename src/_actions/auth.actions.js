1c2dda9537339b74830cc00426fbd62063f05bc7
Facundo-PC <facuperez.net@gmail.com>
2020-05-15 16:21:33 -0300

    fix error

--- src/_actions/auth.actions.js
+++ src/_actions/auth.actions.js
@@ -39,13 +39,14 @@ const register = (props) => {
 
         authService.register(props)
             .then(token => dispatch(setToken(token)))
-            .then(authActions.me)
+            .then(authService.me)
             .then(user => dispatch(setUser(user)))
             .catch(error => dispatch(failure(error)));
     };
 
     function request() { return { type: authConstants.REGISTER_REQUEST } }
     function setToken(token) { return { type: authConstants.LOGIN_SET_TOKEN, token } }
+    function setUser(user) { return { type: authConstants.LOGIN_SET_USER, user } }
     function failure(error) { return { type: authConstants.REGISTER_FAILURE, error } }
 }
 
